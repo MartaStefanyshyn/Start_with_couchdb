6c715e2983862d1c358af4027d46c1d91a4eeb86
Marta Stefanyshyn <martaolehivna@gmail.com>
2015-11-27 17:53:22 +0200

    Display flash messages from server

--- frontend_part/src/app/group/group.new.controller.js
+++ frontend_part/src/app/group/group.new.controller.js
@@ -6,18 +6,38 @@
     .controller('GroupNewController', GroupNewController);
 
   /** @ngInject */
-    function GroupNewController($scope, Groups, $location) {
+    function GroupNewController($scope, Groups, $location, toastr) {
       $scope.createNewGroup = function () {
         function success(response) {
           $scope.response = response;
         }
         function failure(response) {
-          $scope.response = response.data.title[0];
+          console.log(response);
+          angular.forEach(response.data, function(errors, key) {
+            angular.forEach(errors, function(e) {
+              $scope.form[key].$dirty = true;
+              $scope.form[key].$setValidity(e, false);
+            });
+          });
         }
         Groups.create($scope.group, success, failure);
       }
       $scope.back = function () {
         $location.path('/groups');
+      };
+      $scope.errorClass = function(name) {
+        var s = $scope.form[name];
+        return s.$invalid && s.$dirty ? "error" : "";
+      };
+
+      $scope.errorMessage = function(name) {
+        var s = $scope.form[name].$error;
+        var result = [];
+        result.push($scope.form.title.$name);
+        angular.forEach(s, function(key, value) {
+          result.push(value);
+        });
+        return result.join(" ");
       };
     }
 })();
